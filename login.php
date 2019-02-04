e7d26bd06e21c3fbfcce063b1c94d56e20a5f33d
MaryusPeltier <47104640+MaryusPeltier@users.noreply.github.com>
2019-02-04 14:57:03 +0100

    correct first version

--- login.php
+++ login.php
@@ -12,5 +12,10 @@
         <input class="password" type="password" style="border:2px solid gray; border-radius:5px; padding:6px; text-decoration:none; color:black;" placeholder="Mot de passe" name="password">
         <input type="submit" style="border:2px solid gray; border-radius:5px; padding:6px; text-decoration:none; background-color: white; color:gray;" value="Envoyer">
     </form>
+    <?php 
+        if(isset($_GET["message"])){
+            echo $_GET["message"];
+        }
+    ?>
 </body>
 </html>
