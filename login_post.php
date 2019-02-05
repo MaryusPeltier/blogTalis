bbc2c5cd513b415f792df195598dca29f7450797
MaryusPeltier <47104640+MaryusPeltier@users.noreply.github.com>
2019-02-05 10:06:13 +0100

    edit database

--- login_post.php
+++ login_post.php
@@ -1,23 +1,31 @@
 <?php 
+require "database.php";
 
-// var_dump($_GET);
-// var_dump($_POST);
-
-// echo "Bonjour ".$_GET["pseudo"];
-
-// Si pseudo égale à admin et password égale à admin
-    //Message Bonjour
-// SInon redirection login.php
-
-if($_POST["pseudo"] === "admin" && $_POST["password"] === "admin"){
+$req = $db->prepare('SELECT * FROM users WHERE pseudo = :pseudo AND password = :password');
+$req->execute(array(
+    "pseudo" => $_POST["pseudo"],
+    "password" => $_POST["password"]
+));
+$result = $req->fetch();
+if(!$result){
+    header('location:login.php');
+}else{
     session_start();
     $_SESSION["pseudo"] = $_POST["pseudo"];
     header('location:admin.php');
-}else if($_POST["pseudo"] !== "admin"){
-    header('location:login.php?message=Erreur de pseudo');
-}else if($_POST["password"] !== "admin"){
-    header('location:login.php?message=Erreur de password');
 }
-else{
-    header('location:login.php');
-}
+
+
+// // Si pseudo égale à admin et password égale à admin
+// if($_POST["pseudo"] === "admin" && $_POST["password"] === "admin"){
+//     session_start();
+//     $_SESSION["pseudo"] = $_POST["pseudo"];
+//     header('location:admin.php');
+// }else if($_POST["pseudo"] !== "admin"){
+//     header('location:login.php?message=Erreur de pseudo');
+// }else if($_POST["password"] !== "admin"){
+//     header('location:login.php?message=Erreur de password');
+// }
+// else{
+//     header('location:login.php');
+// }
