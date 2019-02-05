c74ba715c55a710f661cf55cd8e5b2a32180b619
MaryusPeltier <47104640+MaryusPeltier@users.noreply.github.com>
2019-02-05 10:44:12 +0100

    add post

--- admin.php
+++ admin.php
@@ -1,21 +1,8 @@
-<?php
-    session_start();
-    
-    if (!isset($_SESSION["pseudo"])){
-        header("Location:login.php");
-    }
+<?php 
+require "header.php";
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <?php require "nav-bar.php"; ?>
     <h1>Admin</h1>
+
+    <a href="add_post.php">Ajouter article</a>
 </body>
 </html>
